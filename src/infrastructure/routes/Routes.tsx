fa6fc01830332e7ecf858d5a4161748af3fd1be1
Danivel Venegas <developer.daniel.venegas@gmail.com>
2023-07-21 21:35:30 -0600

    feat(router): change index for deploy

--- src/infrastructure/routes/Routes.tsx
+++ src/infrastructure/routes/Routes.tsx
@@ -5,14 +5,6 @@ import { RootState } from "../../application/reducers";
 import Login from "../ui/views/Login";
 import MenuList from "../ui/views/MenuList";
 
-const PageNotFound: React.FC = () => {
-  return (
-    <div>
-      <h2>404 Page not found</h2>
-    </div>
-  );
-};
-
 const ProtectedRoute: React.FC<{
   redirectPath?: string;
   children: React.ReactNode;
@@ -28,7 +20,6 @@ const ProtectedRoute: React.FC<{
 const AppRoutes: React.FC = () => {
   return (
     <Routes>
-      {/*<Route path="/" element={<Navigate to="/login" replace />} />*/}
       <Route index path="/" element={<Login />} />
       <Route
         path="products"
@@ -38,7 +29,6 @@ const AppRoutes: React.FC = () => {
           </ProtectedRoute>
         }
       />
-      <Route path="*" element={<PageNotFound />} />
     </Routes>
   );
 };
