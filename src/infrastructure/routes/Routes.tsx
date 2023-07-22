a2beec2096369a1aafa490a5834a2a7a974fa877
Danivel Venegas <developer.daniel.venegas@gmail.com>
2023-07-21 21:20:26 -0600

    feat(router): add 404 for deploy

--- src/infrastructure/routes/Routes.tsx
+++ src/infrastructure/routes/Routes.tsx
@@ -5,6 +5,14 @@ import { RootState } from "../../application/reducers";
 import Login from "../ui/views/Login";
 import MenuList from "../ui/views/MenuList";
 
+const PageNotFound: React.FC = () => {
+  return (
+    <div>
+      <h2>404 Page not found</h2>
+    </div>
+  );
+};
+
 const ProtectedRoute: React.FC<{
   redirectPath?: string;
   children: React.ReactNode;
@@ -30,6 +38,7 @@ const AppRoutes: React.FC = () => {
           </ProtectedRoute>
         }
       />
+      <Route path="*" element={<PageNotFound />} />
     </Routes>
   );
 };
